f2e9a6535d28e028e0ecea0a4bb8fe052abc356d
felicitasmarolda <fmarolda@udesa.edu.ar>
2025-06-09 11:22:34 -0300

    datasets

--- src_data/basic_preprocessing.py
+++ src_data/basic_preprocessing.py
@@ -2,26 +2,30 @@ import numpy as np
 import pandas as pd
 
 def unite_usuarios(csv_2024, csv_2023):
-    """
-    Unites two CSV files containing user data from different years into a single DataFrame.
-    
-    Parameters:
-    csv_2024 (str): Path to the CSV file for the year 2024.
-    csv_2023 (str): Path to the CSV file for the year 2023.
-    
-    Returns:
-    pd.DataFrame: A DataFrame containing the combined user data.
-    """
+
     df_2024 = pd.read_csv(csv_2024)
     df_2023 = pd.read_csv(csv_2023)
 
     # eliminar la última columna de 2023
     if df_2023.shape[1] > 0:
         df_2023 = df_2023.iloc[:, :-1]
+
+    # eliminar primera fila de 2023 (nombres de features)
+    if not df_2023.empty:
+        df_2023 = df_2023.iloc[1:]
     
+    # sacar las comillas de los datos en 2023
+    df_2023 = df_2023.replace('"', '', regex=True)
     
     # Combine the two DataFrames
-    df_combined = pd.concat([df_2024, df_2023], ignore_index=True)
+    # Usar la primera fila de 2024 como nombres de columnas
+    new_columns = df_2024.iloc[0].tolist()
+    df_2024 = df_2024.iloc[1:]
+    df_2024.columns = new_columns
+    df_2023.columns = new_columns
+
+    # Concatenar primero 2023 y luego 2024
+    df_combined = pd.concat([df_2023, df_2024], ignore_index=True)
     
     # Remove duplicate rows
     df_combined.drop_duplicates(inplace=True)
